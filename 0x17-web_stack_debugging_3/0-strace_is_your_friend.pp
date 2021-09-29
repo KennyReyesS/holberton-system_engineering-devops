27d5c7cc66b5e381d5de50dacfbc31ee459bfcdd
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-29 23:48:37 +0000

    comment and made executable

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# This script remplace the class-wp-locale.phpp to class-wp-locale.php in var/www/html/wp-settings.php.
 exec { 'fix-wordpress':
   command => 'sudo sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
