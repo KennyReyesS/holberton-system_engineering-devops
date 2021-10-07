e039fce5c210e044b14b8e3df3d757819215bec0
KennyReyesS <kennyreyes2000@gmail.com>
2021-10-07 01:05:03 -0500

    readme and 0 file created

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Fix Failed requests: 943 to Failed request: 0
+exec { 'fix--for-nginx':
+  command => 'sed -i s/15/4096/g /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
