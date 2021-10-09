3ea3fbd67a1cc1fcfe0a0402c5fda4d29ea63269
KennyReyesS <kennyreyes2000@gmail.com>
2021-10-08 23:46:13 -0500

    nginx restart added

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,8 @@ exec { 'fix--for-nginx':
   command => 'sed -i s/15/4096/g /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+
+exec { 'nginx-restart':
+  command => 'sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
